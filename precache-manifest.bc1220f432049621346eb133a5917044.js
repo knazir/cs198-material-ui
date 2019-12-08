13e371542d0e963dc5e0f81a03223171541bf994
Kashif Nazir <knazir@stanford.edu>
2019-12-07 22:06:41 -0800

    Updates

---
+++ precache-manifest.bc1220f432049621346eb133a5917044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f116906a3cdd3fd7831536873a544805",
+    "url": "/cs198-material-ui/index.html"
+  },
+  {
+    "revision": "54167456bcc5b2f5547a",
+    "url": "/cs198-material-ui/static/js/2.1f372601.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/cs198-material-ui/static/js/2.1f372601.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01ff99c329645f35443a",
+    "url": "/cs198-material-ui/static/js/main.491d2b3b.chunk.js"
+  },
+  {
+    "revision": "7310b866ee8be66cacd4",
+    "url": "/cs198-material-ui/static/js/runtime-main.0b11385c.js"
+  }
+]);
