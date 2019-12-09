aaded9df7594adc1425825d409fe66afb7246c0c
Kashif Nazir <knazir@stanford.edu>
2019-12-08 23:26:18 -0800

    Add title to modal demo

--- src/demo/ModalTest.js
+++ src/demo/ModalTest.js
@@ -5,6 +5,7 @@ import { green } from '@material-ui/core/colors';
 import clsx from 'clsx';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -13,20 +14,21 @@ import Divider from '@material-ui/core/Divider';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import CloseIcon from '@material-ui/icons/Close';
 import ErrorIcon from '@material-ui/icons/Error';
+import NativeSelect from '@material-ui/core/NativeSelect';
 import SendIcon from '@material-ui/icons/Send';
 import IconButton from '@material-ui/core/IconButton';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 import Tooltip from '@material-ui/core/Tooltip';
-import NativeSelect from '@material-ui/core/NativeSelect';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
     marginTop: theme.spacing(4)
+  },
+  header: {
+    marginBottom: theme.spacing(1),
   },
   success: {
     backgroundColor: green[600],
@@ -139,10 +141,10 @@ export default function ModalTest() {
 
   // max width
   const handleMaxWidthChange = e => setState({ modalOpen: true, maxWidth: e.target.value });
-  console.log(state);
 
   return (
-    <div className={classes.root}>
+    <Container className={classes.root} maxWidth="lg">
+      <Typography className={classes.header} variant="h5" component="h2">Modal</Typography>
       <Button variant="outlined" color="primary" onClick={handleModalOpen}>
         Open Partner Request Modal
       </Button>
@@ -150,7 +152,7 @@ export default function ModalTest() {
               maxWidth={state.maxWidth} aria-labelledby="alert-dialog-title"
               aria-describedby="alert-dialog-description">
         <DialogTitle id="alert-dialog-title">Partner Request</DialogTitle>
-        <DialogContent className={classes.modalContent} tabIndex={-1}>
+        <DialogContent className={classes.modalContent}>
           <div className={classes.fieldLine}>
             <div className={classes.widthLabel}>Modal Size:</div>
             <NativeSelect value={state.maxWidth} onChange={handleMaxWidthChange}
@@ -214,6 +216,6 @@ export default function ModalTest() {
         <MySnackbarContentWrapper onClose={handleSnackbarClose} variant="error"
                                   message="Partner request rejected."/>
       </Snackbar>
-    </div>
+    </Container>
   );
 }
